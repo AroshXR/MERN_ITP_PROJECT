92742e417ee063d1d815952a1799725333df1d6b
Pasindu Nuwantha <pasindunuwantha19@gmail.com>
2025-09-20 01:12:21 +0530

    Pasindu - Rental portal

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Define your custom border color here
+        borderColor: '#e0e0e0',  // Replace with your desired color value
+        primary: '#071829ff',
+      },
+    },
   },
   plugins: [],
 }
