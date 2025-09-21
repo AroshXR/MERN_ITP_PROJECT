583065ce18146a195ba6f42e86d4532acaf0dc9b
AroshXR <aroshanasandeep454@gmail.com>
2025-09-21 21:10:14 +0530

    styles fix

--- frontend/src/Components/AdminPanel/AdminPanel.jsx
+++ frontend/src/Components/AdminPanel/AdminPanel.jsx
@@ -2,32 +2,37 @@ import React, { useState } from 'react';
 import AdminApplicantManagement from '../AdminApplicantManagement/AdminApplicantManagement';
 import AdminJobManagement from '../AdminJobManagement/AdminJobManagement';
 import './AdminPanel.css';
+import NavBar from '../NavBar/navBar';
 
 export default function AdminPanel() {
   const [activeTab, setActiveTab] = useState('applicants');
 
   return (
-    <div className="admin-panel">
-      <div className="admin-panel__tabs">
-        <button
-          className={`tab-btn ${activeTab === 'applicants' ? 'active' : ''}`}
-          onClick={() => setActiveTab('applicants')}
-        >
-          Applicants
-        </button>
-        <button
-          className={`tab-btn ${activeTab === 'jobs' ? 'active' : ''}`}
-          onClick={() => setActiveTab('jobs')}
-        >
-          Job Management
-        </button>
-      </div>
+    <div>
+      <NavBar />
+      <div className="admin-panel">
+        <div className="admin-panel__tabs">
+          <button
+            className={`tab-btn ${activeTab === 'applicants' ? 'active' : ''}`}
+            onClick={() => setActiveTab('applicants')}
+          >
+            Applicants
+          </button>
+          <button
+            className={`tab-btn ${activeTab === 'jobs' ? 'active' : ''}`}
+            onClick={() => setActiveTab('jobs')}
+          >
+            Job Management
+          </button>
+        </div>
 
-      <div className="admin-panel__content">
-        {activeTab === 'applicants' && <AdminApplicantManagement />}
-        {activeTab === 'jobs' && <AdminJobManagement />}
+        <div className="admin-panel__content">
+          {activeTab === 'applicants' && <AdminApplicantManagement />}
+          {activeTab === 'jobs' && <AdminJobManagement />}
+        </div>
       </div>
     </div>
+
   );
 }
 
