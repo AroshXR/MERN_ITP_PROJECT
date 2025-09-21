e115d006b0fb4314369d7a654e4354b97c2f80b6
AroshXR <aroshanasandeep454@gmail.com>
2025-09-21 21:02:42 +0530

    contact us ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import AdminJobManagement from './Components/AdminJobManagement/AdminJobManageme
 import AdminApplicantManagement from './Components/AdminApplicantManagement/AdminApplicantManagement';
 import AdminApplicantDetail from './Components/AdminApplicantManagement/AdminApplicantDetail';
 import AdminPanel from './Components/AdminPanel/AdminPanel';
-import ContactUs from './Components/Home/ContactUs';
+import ContactUs from './Components/ContactUsPage/ContactUsPage';
 import OrderManagement from './Components/OrderManagement/OrderManagement';
 import PaymentManagement from './Components/PaymentManagement/PaymentManagement';
 import PaymentDetailsDisplay from './Components/PaymentDetailsDisplay/PaymentDetailsDisplay';
