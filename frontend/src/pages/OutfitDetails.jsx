16b9e0f9c6fb8651b4691968fcc757e75b533a1d
Pasindu Nuwantha <pasindunuwantha19@gmail.com>
2025-09-25 01:10:41 +0530

    paisndu -uotfit details card

--- frontend/src/pages/OutfitDetails.jsx
+++ frontend/src/pages/OutfitDetails.jsx
@@ -124,8 +124,9 @@ const OutfitDetails = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12">
         {/* Left: Outfit Image and Details */}
         <div className="lg:col-span-2">
+          <div className="shadow-xl/30 bg-white rounded-xl p-6 mb-6 -ml-6 md:-ml-6 lg:-ml-16 xl:-ml-24">
           {/* Image Slider */}
-          <div className="flex items-center -ml-20 mr-6">
+          <div className="flex items-center -ml-6 mr-4">
             <button onClick={prevSlide} className="md:p-2 p-1 bg-black/30 md:mr-6 mr-2 rounded-full hover:bg-black/50">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
@@ -168,6 +169,8 @@ const OutfitDetails = () => {
                   {text}
                 </div>
               ))}
+            </div>
+
             </div>
 
             {/* Description */}
